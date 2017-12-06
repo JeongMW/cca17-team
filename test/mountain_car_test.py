b8ea09204a6f45c6f1c3bdedad6ec80e12a004b9
JeongMW <jmw95217@gmail.com>
2017-12-06 13:48:44 +0900

    make test folder for testing

---
+++ test/mountain_car_test.py
@@ -0,0 +1,19 @@
+import gym
+
+env = gym.make("BreakoutDeterministic-v4")
+
+def main():
+    max_episodes = 50
+
+    for episode in range(max_episodes):
+        
+        state = env.reset()
+        done = False
+
+        while not done:
+            env.render()
+            action = env.action_space.sample()
+            next_state, reward, done, _ = env.step(action)
+
+if  __name__ == "__main__":
+    main()
